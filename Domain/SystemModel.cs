bf8a14c3c287ce8f1a07e6d38fc397fa9e767eb6
唐富伟 <tianskyluj@sina.com>
2013-09-11 17:41:40 +0800

    添加系统模块类

---
+++ Domain/SystemModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace Domain
+{
+    public class SystemModel
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public virtual Guid ID { get; set; }
+
+        /// <summary>
+        /// 模块名称
+        /// </summary>
+        public virtual string Name { get; set; }
+
+        /// <summary>
+        /// 路径
+        /// </summary>
+        public virtual string Url { get; set; }
+
+        /// <summary>
+        /// 上级节点
+        /// </summary>
+        public virtual int ParentId { get; set; }
+
+        /// <summary>
+        /// 是否有效
+        /// </summary>
+        public virtual bool IsEnabled { get; set; }
+    }
+}
